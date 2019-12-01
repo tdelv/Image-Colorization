25a5ddc2be27782dd1600f4b132f575657334594
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2019-11-30 21:26:42 -0500

    Added main loading function for training data.

--- data/training_preprocess.py
+++ data/training_preprocess.py
@@ -1,4 +1,30 @@
+import torch
+import itertools
 
+def load_data(train_dataset="", batch_size=100, shuffle=True):
+	imagenet_data = torchvision.datasets.ImageNet('./hymenoptera_data')
+	train_loader = torch.utils.data.DataLoader(
+        imagenet_data, batch_size=batch_size, shuffle=shuffle)
+
+	train_loader_lab = map(imagenet_to_lab, img)
+	loader1, loader2, loader3, loader4 = itertools.tee(train_loader_lab, 4)
+
+	train_loader_inputs = map(lab_to_bw, loader1)
+	train_loader_local_hints = map(generate_local_hints, loader2)
+	train_loader_global_hints = map(generate_global_hints, loader3)
+	train_loader_labels = map(generate_label, loader4)
+
+    '''
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+    '''
 
 def imagenet_to_lab(img):
 	"""
